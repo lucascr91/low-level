9709abef9079aebdf200e5d198f9f20145681fba
lucascr91 <lucas.ecomg@gmail.com>
2021-06-25 22:08:54 -0300

    Update blog

--- rust2pythonmodules/vigenere.rs
+++ rust2pythonmodules/vigenere.rs
@@ -1,24 +1,37 @@
-const LETTERS: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LETTERS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 fn main() {
-    println!("{}", translate_message(String::from("PIZZA"), String::from("Lucas"), String::from("encrypt")))
+    println!("{:?}", translate_message("PIZZA", "Lucas", "encrypt"))
 }
 
-fn translate_message(key: String, message: String, mode String) {
-    let mut translate: Vec<String> = Vec::new();
+fn translate_message(key: &str, message: &str, mode: &str) -> String {
+    let mut translate: Vec<char> = Vec::new();
     let mut keyIndex = 0;
-    let mut key = key.make_ascii_uppercase();
+    let mut key = key.to_uppercase();
 
     for symbol in message.chars() {
-        let mut num = LETTERS.find(symbol);
-        if num.is_some() {
-            if mode == 'encrypt' {
-                num+=LETTERS.find(key[keyIndex]).unwrap();
-            } else if {
-                num+=LETTERS.find(key[keyIndex]).unwrap();
+        let mut num = LETTERS.find(symbol).unwrap();
+        if num>=0 {
+            if mode == "encrypt" {
+                num+=LETTERS.find(key.chars().nth(keyIndex).unwrap()).unwrap();
+            } else if mode == "decrypt" {
+                num+=LETTERS.find(key.chars().nth(keyIndex).unwrap()).unwrap();
             }
-        }
-        if 
-    }
 
+            if symbol.is_uppercase() {
+                translate.push(LETTERS.chars().nth(num).unwrap());
+            } else if symbol.is_uppercase() {
+                translate.push(LETTERS.chars().nth(num).unwrap().to_lowercase())
+            }
+
+            keyIndex+=1;
+            if keyIndex == (key.chars().collect::<Vec<char>>().len()) {
+                keyIndex = 0;
+            }
+        } else {
+            translate.push(symbol)
+        }
+    }
+    let result: String = translate.into_iter().collect();
+    return result
 }
