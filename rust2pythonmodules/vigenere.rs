c47de8f3ec6b33be050c1df1f9ac8362857cef92
lucascr91 <lucas.ecomg@gmail.com>
2021-06-25 22:42:40 -0300

    Update blog

--- rust2pythonmodules/vigenere.rs
+++ rust2pythonmodules/vigenere.rs
@@ -6,27 +6,28 @@ fn main() {
 
 fn translate_message(key: &str, message: &str, mode: &str) -> String {
     let mut translate: Vec<char> = Vec::new();
-    let mut keyIndex = 0;
-    let mut key = key.to_uppercase();
+    let mut key_index = 0;
+    let key = key.to_uppercase();
+    let message = message.to_uppercase();
 
     for symbol in message.chars() {
         let mut num = LETTERS.find(symbol).unwrap();
         if num>=0 {
             if mode == "encrypt" {
-                num+=LETTERS.find(key.chars().nth(keyIndex).unwrap()).unwrap();
+                num+=LETTERS.find(key.chars().nth(key_index).unwrap()).unwrap();
             } else if mode == "decrypt" {
-                num+=LETTERS.find(key.chars().nth(keyIndex).unwrap()).unwrap();
+                num-=LETTERS.find(key.chars().nth(key_index).unwrap()).unwrap();
             }
 
             if symbol.is_uppercase() {
                 translate.push(LETTERS.chars().nth(num).unwrap());
-            } else if symbol.is_uppercase() {
-                translate.push(LETTERS.chars().nth(num).unwrap().to_lowercase())
+            } else if symbol.is_lowercase() {
+                translate.push(LETTERS.chars().nth(num).unwrap().to_lowercase().collect::<Vec<char>>()[0])
             }
 
-            keyIndex+=1;
-            if keyIndex == (key.chars().collect::<Vec<char>>().len()) {
-                keyIndex = 0;
+            key_index+=1;
+            if key_index == (key.chars().collect::<Vec<char>>().len()) {
+                key_index = 0;
             }
         } else {
             translate.push(symbol)
